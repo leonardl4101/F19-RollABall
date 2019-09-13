6bf336db3b170d0e9b0613a7c79624f884e53bf8
leonardl4101 <ll788@scarletmail.rutgers.edu>
2019-09-12 22:32:32 -0400

    Step1 add player motion script

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+   void Start() 
+
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Verticle");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
+
+        rb.AddForce(movement);
+    }
+
+      
+}
