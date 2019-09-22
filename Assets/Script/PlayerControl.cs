7c6b6d413e050eea4f3c101487a323f18c08784b
Leonard Leyva <ll788@scarletmail.rutgers.edu>
2019-09-22 17:49:09 -0400

    You Win Text

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerControl : MonoBehaviour
 {
     public float speed;
     public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
     private int count;
@@ -15,6 +16,7 @@ public class PlayerControl : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText ();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -38,6 +40,10 @@ public class PlayerControl : MonoBehaviour
      void SetCountText()
     {
         countText.text = "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 
 }
