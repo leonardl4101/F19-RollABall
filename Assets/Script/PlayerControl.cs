7c64d33d40074c3338271098912b2656662883fa
Leonard Leyva <ll788@scarletmail.rutgers.edu>
2019-09-22 17:41:01 -0400

    Win Text

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
     public float speed;
+    public Text countText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        count = 0;
+        SetCountText ();
     }
     void FixedUpdate()
     {
@@ -28,7 +31,15 @@ public class PlayerControl : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
         }
     }
+     void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString ();
+    }
+
 }
+
 
